c357b1564220176cff43d72413720eb4cc383911
Luqman Rasat <luqmanrasat@hotmail.com>
2022-09-02 03:40:49 +0800

    loop savedrepos card

--- src/pages/SavedRepos.tsx
+++ src/pages/SavedRepos.tsx
@@ -1,11 +1,17 @@
-import { Component } from 'solid-js';
+import { Component, createSignal, For } from 'solid-js';
+import RepoCard from '../components/RepoCard';
+import { Repo } from '../types';
+
+const [savedRepos, setSavedRepos] = createSignal<Repo[]>([]);
 
 const SavedRepos: Component = () => {
   return (
     <div>
-      <h2>SavedRepos</h2>
+      <h2>Your Saved Repos</h2>
+      <For each={savedRepos()}>{(repo) => <RepoCard repo={repo} />}</For>
     </div>
-  )
-}
+  );
+};
 
 export default SavedRepos;
+export { setSavedRepos };
