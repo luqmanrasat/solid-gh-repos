03cf6d7ab7cb0a86e7d6a19a1b1de8f7ba809b71
Luqman Rasat <luqmanrasat@hotmail.com>
2022-09-01 18:22:34 +0800

    refetch different username

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,11 +1,34 @@
-import { Component } from 'solid-js';
+import { Component, createEffect } from "solid-js";
+import { repos, setUsername, username } from "../App";
 
 const Home: Component = () => {
+  let usernameInput: HTMLInputElement;
+
+  const refetchRepos = (event: Event) => {
+    event.preventDefault();
+    setUsername(usernameInput.value);
+  };
+
+  createEffect(() => {
+    console.log(repos());
+  });
+
   return (
     <div>
-      <h2>Home</h2>
+      <form class="mb-3" onSubmit={(event) => refetchRepos(event)}>
+        <input
+          ref={usernameInput!}
+          class="p-1 align-middle"
+          type="text"
+          required
+        />
+        <button class="btn btn-dark align-middle ml-3" type="submit">
+          Fetch
+        </button>
+      </form>
+      <h3>Github repos for {username()}</h3>
     </div>
-  )
-}
+  );
+};
 
 export default Home;
